b3251961322d87ddb32fbb7e0e32b277c93c37bb
Sagar Mane <sagar.mane006@gmail.com>
2017-05-05 15:08:33 -0700

    Adding source code for tenant 3 server

---
+++ src/Tenant3-Server/generate_uml.py
@@ -0,0 +1,15 @@
+from subprocess import *
+
+def jarWrapper(*args):
+    process = Popen(['java', '-cp']+list(args), stdout=PIPE, stderr=PIPE)
+    retString = []
+    while process.poll() is None:
+        line = process.stdout.readline()
+        if line != '' and line.endswith('\n'):
+            retString.append(line[:-1])
+    stdout, stderr = process.communicate()
+    retString += stdout.split('\n')
+    if stderr != '':
+        retString += stderr.split('\n')
+        retString.remove('')
+    return retString
