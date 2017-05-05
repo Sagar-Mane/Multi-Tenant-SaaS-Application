baaea4cb80b61491f80bff00380348826825b876
Sagar Mane <sagar.mane006@gmail.com>
2017-05-05 10:02:56 -0700

    Image display bug fix and tenant 2 UI added

--- src/Multi-Tenant-SaaS-Application-Portal/public/angular/main.js
+++ src/Multi-Tenant-SaaS-Application-Portal/public/angular/main.js
@@ -69,6 +69,157 @@ app.controller('tenant1_controller', ['$scope','$http', 'fileUpload', function($
         console.log("Reporting from generateUML POST Angular");
         $http({
             method: 'POST',
+            url: "http://35.164.133.164:90/generateUML",
+        }).success(function(data){
+            $scope.getUML();
+            })
+	   };
+	   $scope.getUML=function(){
+
+	        $http({
+            method: 'GET',
+            url: "http://35.164.133.164:90/getUML",
+        }).success(function(data){
+                console.log(data.result);
+            $scope.image=data.result;
+            $scope.showuml=true;
+	        $scope.showgrade=true;
+        })
+	   }
+
+	    $scope.grade=function(){
+	        console.log("Reporting from grade POST Angular");
+	        console.log(document.getElementById("complete").checked);
+	        console.log("Points---",$scope.points);
+	        console.log("Comments--",$scope.comments);
+	    };
+
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log("Reporting fro uploadFile POST Angular");
+        var uploadUrl = "http://35.164.133.164:90/uploadCode";
+        fileUpload.uploadFileToUrl(file, uploadUrl, function(data) {
+        	console.log(data.Upload_Code_Status);
+        	$scope.upload_status=true;
+        	$scope.status_code = data.status;
+        });
+    };
+}]);
+
+
+
+/*-----------------------------------------Tenant 2 Controller---------------------------------------------------------------------------------*/
+app.controller('tenant2_controller', ['$scope','$http', 'fileUpload', function($scope,$http, fileUpload){
+
+    console.log("Reporting from Tenant 2 controller");
+	$scope.showuml=false;
+	$scope.showgrade=false;
+	$scope.upload_status=false;
+
+	    $scope.generateUML=function(){
+
+        console.log("Reporting from generateUML POST Angular");
+        $http({
+            method: 'POST',
+            url: "http://127.0.0.1:80/generateUML",
+        }).success(function(data){
+            $scope.getUML();
+            })
+	   };
+	   $scope.getUML=function(){
+
+	        $http({
+            method: 'GET',
+            url: "http://127.0.0.1:80/getUML",
+        }).success(function(data){
+            $scope.image=data.result;
+            $scope.showuml=true;
+	        $scope.showgrade=true;
+        })
+	   }
+
+	    $scope.grade=function(){
+	        console.log("Reporting from grade POST Angular");
+	        console.log("Points---",$scope.points);
+	        console.log("Comments--",$scope.comments);
+	    };
+
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log("Reporting fro uploadFile POST Angular");
+        var uploadUrl = "http://127.0.0.1:80/uploadCode";
+        fileUpload.uploadFileToUrl(file, uploadUrl, function(data) {
+        	console.log(data.Upload_Code_Status);
+        	$scope.upload_status=true;
+        	$scope.status_code = data.status;
+        });
+    };
+}]);
+/*-------------------------------------------------Tenant 3 Controller------------------------------------------------------------------------*/
+    app.controller('tenant3_controller', ['$scope','$http', 'fileUpload', function($scope,$http, fileUpload){
+
+    console.log("Reporting from Tenant 3 controller");
+	$scope.showuml=false;
+	$scope.showgrade=false;
+	$scope.upload_status=false;
+
+	    $scope.generateUML=function(){
+
+        console.log("Reporting from generateUML POST Angular");
+        $http({
+            method: 'POST',
+            url: "http://127.0.0.1:5000/generateUML",
+        }).success(function(data){
+            $scope.getUML();
+            })
+	   };
+	   $scope.getUML=function(){
+
+	        $http({
+            method: 'GET',
+            url: "http://127.0.0.1:5000/getUML",
+        }).success(function(data){
+                console.log(data);
+            $scope.image=data.result;
+            $scope.showuml=true;
+	        $scope.showgrade=true;
+        })
+	   }
+
+	    $scope.grade=function(){
+	        console.log("Reporting from grade POST Angular");
+	        console.log(document.getElementById("complete").checked);
+	        console.log("Points---",$scope.points);
+	        console.log("Comments--",$scope.comments);
+	    };
+
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log("Reporting fro uploadFile POST Angular");
+        var uploadUrl = "http://127.0.0.1:5000/uploadCode";
+        fileUpload.uploadFileToUrl(file, uploadUrl, function(data) {
+        	console.log(data.Upload_Code_Status);
+        	$scope.upload_status=true;
+        	$scope.status_code = data.status;
+        });
+    };
+}]);
+/*------------------------------------------------------------------Tenant 4 Controller--------------------------------------------------------*/
+    
+    
+    
+    app.controller('tenant4_controller', ['$scope','$http', 'fileUpload', function($scope,$http, fileUpload){
+
+    console.log("Reporting from Tenant 4 controller");
+	$scope.showuml=false;
+	$scope.showgrade=false;
+	$scope.upload_status=false;
+
+	    $scope.generateUML=function(){
+
+        console.log("Reporting from generateUML POST Angular");
+        $http({
+            method: 'POST',
             url: "http://127.0.0.1:5000/generateUML",
         }).success(function(data){
             $scope.getUML();
@@ -104,17 +255,4 @@ app.controller('tenant1_controller', ['$scope','$http', 'fileUpload', function($
         });
     };
 }]);
-
-app.controller("tenant2_controller",function($scope,$http){
-	console.log("Reporting from Tenant 2 controller");
-	});
-app.controller("tenant3_controller",function($scope,$http){
-	console.log("Reporting from Tenant 3 controller");
-	});
-
-app.controller("tenant4_controller",function($scope,$http){
-	console.log("Reporting from Tenant 4 controller");
-	});
-
-
 
